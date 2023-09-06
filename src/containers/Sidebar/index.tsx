94544c804f6b3c97085d7e2c5236a46b2ccb0968
Thales Pires <thaedupi@gmail.com>
2023-09-06 20:30:00 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ThalesEPires.png" />
-    <Titulo fontSize={20}>Thales Pires</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Thales Pires</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ThalesEPires
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
