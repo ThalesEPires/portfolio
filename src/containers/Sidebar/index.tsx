04a2f134d1c8a2c80ca3e8724ce4a188567f0f0f
Thales Pires <thaedupi@gmail.com>
2023-09-06 19:24:44 -0300

    construindo a estrutura e a seção 'Sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/ThalesEPires.png" />
+    <Titulo fontSize={20}>Thales Pires</Titulo>
+  </aside>
+)
+
+export default Sidebar
